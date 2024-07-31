18ef49dfa571a3941e0f3cada906e68dc1a309ff
Fabricyo Vargas Mattivi <fabricyo98@hotmail.com>
2024-07-31 16:41:36 -0300

    Primeira versão do Portfólio

---
+++ MeuPortfolio/js/script.js
@@ -0,0 +1,98 @@
+let menuHamburguer = document.querySelector('.menu-hamburguer');
+let btnMaisProjetos = document.querySelector('.btn-mais-projetos');
+let lerMais = document.querySelector('#ler-mais');
+let navItem = document.querySelectorAll('.navegacao a[href^="#"]');
+let btnTema = document.querySelector('.btn-tema');
+
+
+/* ------------- Funções ------------- */
+function getDistanceFromTheTop(elemento) {
+    const id = elemento.getAttribute("href");
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollSuave(distanciaDoTopo) {    
+    window.scroll({
+        top: distanciaDoTopo,
+        behavior: "smooth",
+    });  
+    ocultarSideMenu();  
+}
+
+function scrollToSection(event) {
+    event.preventDefault();   
+    const distanciaDoTopo = getDistanceFromTheTop(event.target) - 100;
+    console.log(distanciaDoTopo);
+    scrollSuave(distanciaDoTopo);
+}
+
+function ocultarSideMenu() {
+    let nav = document.querySelector('.navegacao');
+
+    if(menuHamburguer.classList.contains('ativado')) {
+        menuHamburguer.classList.remove('ativado');
+        nav.classList.remove('side-menu');
+    }
+}
+
+
+/* ------------- Scroll Suave ------------- */
+navItem.forEach((link) => {
+    link.addEventListener('click', scrollToSection);    
+});
+
+
+/* ------------- Menu Hamburguer ------------- */
+menuHamburguer.addEventListener('click', () => {
+    let nav = document.querySelector('.navegacao');
+
+    menuHamburguer.classList.toggle('ativado');
+
+    if(menuHamburguer.classList.contains('ativado')) {
+        nav.classList.add('side-menu');
+    }
+    else {
+        nav.classList.remove('side-menu');
+    }
+});
+
+
+/* ------------- Botão "Ver Mais" em 'Projetos' ------------- */
+btnMaisProjetos.addEventListener('click', () => {
+    let section = document.querySelector('.section-projetos');
+
+    section.classList.toggle('exibir-projetos');
+    
+
+    if (section.classList.contains('exibir-projetos')) {
+        return btnMaisProjetos.textContent = "Ver menos";
+    }
+
+    btnMaisProjetos.textContent = "Ver mais";
+});
+
+
+/* ------------- Botão "Ler mais" em 'Sobre Mim' ------------- */
+lerMais.addEventListener('click', () => {
+    let textoEscondido = document.querySelector('.texto-escondido');
+    let pontinhos = document.querySelector('#pontinhos');
+
+    textoEscondido.classList.toggle('exibir-texto');
+
+    if(textoEscondido.classList.contains('exibir-texto')) {
+        lerMais.textContent = "Ler menos";
+        pontinhos.textContent = ".";
+    }
+    else {
+        lerMais.textContent = "Ler mais";
+        pontinhos.textContent = "...";
+    }    
+});
+
+
+/* ------------- Botão Para Alternar Modo Dark/Light ------------- */
+btnTema.addEventListener('click', () => {   
+    let containerPrincipal = document.querySelector('.container-principal');
+
+    containerPrincipal.classList.toggle('modo-light');
+});
